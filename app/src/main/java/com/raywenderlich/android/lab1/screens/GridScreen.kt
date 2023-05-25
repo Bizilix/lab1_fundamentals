bed2932d79fbf9843810d8317178c88dd638a8c7
Bizilix <gleb.moz@mail.ru>
2023-05-25 17:21:29 +0300

    GridScreen.kt

--- app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
@@ -1,4 +1,4 @@
-package com.raywenderlich.android.lab1.screens
+package com.topic2.android.notes
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Row
@@ -37,24 +37,77 @@ private val items = listOf(
     Icons.Filled.Build,
     Icons.Filled.ThumbUp,
 )
+
 @ExperimentalFoundationApi
 @Composable
 fun GridScreen(){
     LazyVerticalGrid(
         modifier = Modifier.fillMaxSize(),
-        columns = GridCells.Fixed(3),
-
+        columns = GridCells.Fixed(3) ,
         content = {
-            items(count = items.size){ item ->
+            items(count = items.size){item ->
                 GridIcon(IconResource(items[item], true))
-
             }
         }
-
     )
+
     BackButtonHandler {
         FundamentalsRouter.navigateTo(Screen.Navigation)
     }
+}
+
+@Composable
+fun GridView(columnCount: Int) { val itemSize = items.size
+    val rowCount = ceil(itemSize.toFloat() / columnCount).toInt()
+    val gridItems = mutableListOf<List<IconResource>>()
+    var position = 0
+
+    for (i in 0 until rowCount) {
+        val rowItem = mutableListOf<IconResource>()
+        for (j in 0 until columnCount) {
+            if (position.inc() <= itemSize) {
+                rowItem.add(IconResource(items[position++], true))
+            }
+        }
+        // here
+        val itemsToFill = columnCount - rowItem.size
+
+        for (j in 0 until itemsToFill) {
+            rowItem.add(IconResource(Icons.Filled.Delete, false))
+        }
+        gridItems.add(rowItem)
+    }
+    // here
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(gridItems) { items ->
+            RowItem(items)
+        }
+    }
+}
+
+@Composable
+fun RowItem(rowItems: List<IconResource>) {
+    Row {
+        for (element in rowItems)
+            GridIcon(element)
+    }
+}
+
+@Composable
+fun RowScope.GridIcon(iconResource: IconResource){
+    val color = if (iconResource.isVisible)
+        colorResource(R.color.colorPrimary)
+    else Color.Transparent
+
+    Icon(
+        imageVector = iconResource.imageVector,
+        tint = color,
+        contentDescription = stringResource(R.string.grid_icon),
+        modifier = Modifier
+            .size(80.dp, 80.dp)
+            .weight(1f)
+
+    )
 }
 
 @Composable
@@ -72,57 +125,4 @@ fun GridIcon(iconResource: IconResource){
     )
 }
 
-@Composable
-fun GridView(columnCount:Int) { val itemSize= items.size
-    val rowCount = ceil(itemSize.toFloat() / columnCount).toInt()
-    val gridItems = mutableListOf<List<IconResource>>()
-    var position = 0
-
-    for (i in 0 until rowCount){
-        val rowItem = mutableListOf<IconResource>()
-        for (j in 0 until columnCount) {
-            if  (position.inc() <= itemSize){
-                rowItem.add(IconResource(items[position++],true))
-
-
-                val itemsToFill  = columnCount - rowItem.size
-                for (j in 0 until itemsToFill){
-                    rowItem.add(IconResource(Icons.Filled.Delete, false))
-                }
-                gridItems.add(rowItem)
-                LazyColumn(modifier =  Modifier.fillMaxSize()) {
-                    items(gridItems) { items ->
-                        RowItem(items)
-                    }
-                }
-            }
-
-        }
-    }
-}
-
-
-@Composable
-fun RowItem(rowItems: List<IconResource>){
-    Row {
-        for (element in rowItems)
-            GridIcon(element)
-    }
-}
-
-@Composable
-fun RowScope.GridIcon(iconResource:IconResource) {
-    val color = if (iconResource.isVisible)
-        colorResource(R.color.colorPrimary)
-    else Color.Transparent
-
-    Icon(
-        imageVector = iconResource.imageVector,
-        tint = color,
-        contentDescription = stringResource(R.string.grid_icon),
-        modifier = Modifier
-            .size(80.dp, 80.dp)
-            .weight(1f)
-    )
-}
 data class IconResource(val imageVector: ImageVector, val isVisible:Boolean)
